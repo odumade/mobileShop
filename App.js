21ed90e941ffa8d37ca89082ef673575c259276c
odumade <104792577+odumade@users.noreply.github.com>
2022-07-12 21:42:59 +0100

    Initial commit with partial folder structure

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native';
+import ProductContainer from './src/Screens/Products/ProductContainer';
+
+
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <ProductContainer />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
+
+export default App;
---
+++ src/Screens/Products/ProductContainer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { View, Text, ActivityIndicator, SafeAreaView, StyleSheet } from 'react-native'
+
+const ProductContainer = () => {
+    return (
+        <View>
+            <Text>Product Container</Text>
+        </View>
+    )
+}
+
+
+
+export default ProductContainer
