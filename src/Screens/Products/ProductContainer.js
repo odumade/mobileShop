21ef1e983023f7d6c98812c7cef9dc05972499c9
odumade <104792577+odumade@users.noreply.github.com>
2022-07-12 22:00:39 +0100

    Displayed product brand name using flatlist horizontally

--- src/Screens/Products/ProductContainer.js
+++ src/Screens/Products/ProductContainer.js
@@ -1,14 +1,35 @@
-import React, { useState } from 'react'
-import { View, Text, ActivityIndicator, SafeAreaView, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text, ActivityIndicator, SafeAreaView, StyleSheet, FlatList } from 'react-native';
+
+const data = require('../../../Constants/products.json');
 
 const ProductContainer = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(data);
+        return () => {
+            setProducts([])
+        }
+    }, []);
+
+
     return (
         <View>
             <Text>Product Container</Text>
+            <View style={{ marginTop: 100 }}>
+                <FlatList
+                    horizontal
+                    data={products}
+                    renderItem={({ item }) => <Text>{item.brand}</Text>}
+                    keyExtractor={item => item.name}
+                />
+            </View>
         </View>
     )
 }
 
 
 
-export default ProductContainer
+export default ProductContainer;
